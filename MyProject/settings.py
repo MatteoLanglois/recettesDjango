cc07f8a930b029249372b6af59e88f8e172fb461
matteolanglois <langlois.matteo49@gmail.com>
2022-07-04 14:35:11 +0200

    update models (with has)

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1qu$i#8a88ue5%v(rrd(8vus*)&c=n7wh8qe)(+odq+b-^mo#*'
+with open('../secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
